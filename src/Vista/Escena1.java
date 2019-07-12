f30c694569daaebc080eab148bf1f3bb0f8f7478
usuario1 <usuario1@DESKTOP-JFS3G0S>
2019-07-11 22:29:12 -0500

    firsth commit

---
+++ src/Vista/Escena1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+import javafx.scene.Scene;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Escena1 implements Vista{
+
+    private Scene escena;
+    
+    
+    @Override
+    public Scene getScene() {
+        return this.escena;   
+    }
+    
+}
---
+++ src/mvc/MVC.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+import controlador.Controlador1;
+import controlador.Singleton;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import vista.Escena1;
+import vista.Vista;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class MVC extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+    
+}
